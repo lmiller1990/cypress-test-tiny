6a7ed75c9489bd8e47cfa93b9b4821db4814632e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-12 19:33:12 +1000

    fail to repro

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,32 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {},
+// cypress.config.ts
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  reporter: "mocha-junit-reporter",
+
+  reporterOptions: {
+    mochaFile: "cypress/reports/junit/test-results.[hash].xml",
+    testsuitesTitle: false,
   },
-}
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    // https://docs.cypress.io/guides/references/migration-guide#Plugins-File-Removed
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:8082/",
+    specPattern: "cypress/e2e/**/*.feature",
+  },
+
+  component: {
+    supportFile: false,
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
---
+++ cypress/plugins/index.js
@@ -0,0 +1,24 @@
+// cypress/plugins/index.ts
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const browserify = require('@cypress/browserify-preprocessor');
+
+module.exports = (on, config) => {
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: require.resolve('typescript'),
+  };
+
+  options.browserifyOptions.plugin.unshift([
+    'tsify',
+    { project: 'cypress/tsconfig.json' },
+  ]);
+
+  on('file:preprocessor', browserify());
+  on('file:preprocessor', cucumber(options));
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome') {
+      launchOptions.args.push('--disable-dev-shm-usage');
+    }
+    return launchOptions;
+  });
+};

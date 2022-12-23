6bf49431476655d1518e5432c1385d435c322ec5
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-23 13:41:50 +1000

    reproduction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:8000')
+    cy.get('#app').contains('hello')
   })
 })

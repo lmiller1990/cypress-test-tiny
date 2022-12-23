d102cbc28216d6399924c4c992dcfe3de293b114
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-23 13:48:58 +1000

    repro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,14 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('http://localhost:8000')
-    cy.get('#app').contains('hello')
+    cy.get('#app').contains('hello').then(() => {
+      cy.window().then(win => {
+        const resources = win.performance.getEntriesByType("resource");
+        console.log('Here', resources)
+        resources.forEach((entry) => {
+          console.log(entry);
+        });
+      })
+    })
   })
 })

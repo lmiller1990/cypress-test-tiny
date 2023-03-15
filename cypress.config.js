37848d6c0ce2fa7da3d9eb4e15cd4bc46cd6edad
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-03-15 15:27:11 +1000

    repro

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {},
+  reporter: "cypress-multi-reporters",
+
+  reporterOptions: {
+    configFile: "reporters/custom.json",
   },
-}
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};

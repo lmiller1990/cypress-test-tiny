0ed0f327f7587dcaf6cf5bc948deb912adb47e92
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-03-21 15:01:14 +1000

    repro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+let firstAttempt = true
+
+beforeEach(() => {
+  if (firstAttempt) throw new Error('Something went wrong')
 })
+
+afterEach(() => {
+  if (firstAttempt) {
+    firstAttempt = false
+    throw new Error('Something went wrong')
+  }
+})
+
+it(
+  'Cypress repeats attempt lines when both beforeEach and afterEach fail',
+  { retries: 2 },
+  () => {}
+)

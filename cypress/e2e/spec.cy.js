62b921e43d0196160a40d28f3e460e51256f0c53
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-03-27 18:18:31 +1000

    fix it

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,7 +9,7 @@ describe('page', { defaultCommandTimeout: 1000 * 20 }, () => {
   for (const n of [1,2,3,4]) {
     it('fails slowly', () => {
       cy.wait(1000 * 18)
-      expect(2).to.eq(n)
+      expect(2).to.eq(2)
     })
   }
 })

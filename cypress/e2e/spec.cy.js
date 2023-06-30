1498605c05133cfec393afcce72c2e074013b002
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-06-30 10:44:58 +1000

    wip

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,7 @@
 /// <reference types="cypress" />
-describe('page', { defaultCommandTimeout: 1000 * 20 }, () => {
+describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
-    // testing
-    // cy.wait(1000 * 60)
+    cy.visit('http://localhost:8000')
+    cy.get('#menu').should('be.visible')
   })
-
-  for (const n of [1,2,3,4]) {
-    it('fails slowly', () => {
-      cy.wait(1000 * 18)
-      expect(2).to.eq(2)
-    })
-  }
 })

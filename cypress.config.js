d51f21207daf7a341907a4919047883ec29a16f6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-07-10 09:25:41 +1000

    wip

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   projectId: '5tbaxy',
   e2e: {
+    experimentalModifyObstructiveThirdPartyCode: true,
     setupNodeEvents(on, config) {},
   },
 }
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,16 @@
-/// <reference types="cypress" />
-describe('page', { defaultCommandTimeout: 1000 * 20 }, () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-    // testing
-    // cy.wait(1000 * 60)
-  })
+describe("User is on Comptia Login Page", () => {
+  it("works", () => {
+    cy.visit("https://prep.comptia.org/en/");
+    cy.get("a").contains("Sign in").click({ force: true });
 
-  for (const n of [1,2,3,4]) {
-    it('fails slowly', () => {
-      cy.wait(1000 * 18)
-      expect(2).to.eq(2)
-    })
-  }
-})
+    cy.origin("https://comptiassostageb2c01.b2clogin.com/", () => {
+      // Here after this click domain change to Google OAuth
+      cy.get(".options #GoogleExchange").click();
+    });
+
+    cy.origin("https://accounts.google.com", () => {
+      cy.url().should("include", "accounts.google.com");
+      cy.get('input[type="email"]').click(); //Here trying to enter email on Google OAuth page
+    });
+  });
+});

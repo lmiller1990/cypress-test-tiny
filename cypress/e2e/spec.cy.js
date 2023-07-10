c6300a8b003d6c41a4b9b975f55fccae3f0a95a8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-07-11 09:52:03 +1000

    ok

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,10 @@
-/// <reference types="cypress" />
-describe('page', { defaultCommandTimeout: 1000 * 20 }, () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-    // testing
-    // cy.wait(1000 * 60)
-  })
+describe("page", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
 
-  for (const n of [1,2,3,4]) {
-    it('fails slowly', () => {
-      cy.wait(1000 * 18)
-      expect(2).to.eq(2)
-    })
-  }
-})
+  it("works", () => {
+    cy.visit("localhost:8000/index.html");
+    cy.get("button").click();
+  });
+});
